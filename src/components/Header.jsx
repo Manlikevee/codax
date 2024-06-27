f0c853973dba776822d08f62ce6f9710f55664b3
manlikevee <odahviktor@gmail.com>
2024-06-27 18:05:56 +0100

    sacsac

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,58 +1,57 @@
 'use client'
+
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
+import Cookies from 'js-cookie'
 import Image from 'next/image'
 import myimg from '../../public/trello.png'
+
 const Header = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
-
-  const [isDarkMode, setIsDarkMode] = useState(() => {
-    const savedMode = localStorage.getItem('darkMode');
-    return savedMode ? JSON.parse(savedMode) : true; // Default to dark mode
-  });
+  useEffect(() => {
+    const savedMode = Cookies.get('darkMode');
+    if (savedMode !== undefined) {
+      setIsDarkMode(JSON.parse(savedMode));
+      document.body.classList.toggle('dark-mode', JSON.parse(savedMode));
+      document.body.classList.toggle('light-mode', !JSON.parse(savedMode));
+    }
+  }, []);
 
   // Function to toggle dark mode
   const toggleDarkMode = () => {
     const newMode = !isDarkMode;
     setIsDarkMode(newMode);
-    localStorage.setItem('darkMode', JSON.stringify(newMode));
+    Cookies.set('darkMode', JSON.stringify(newMode), { expires: 365 });
     document.body.classList.toggle('dark-mode', newMode);
     document.body.classList.toggle('light-mode', !newMode);
   };
 
-  // Apply the mode on component mount
-  useEffect(() => {
-    document.body.classList.toggle('dark-mode', isDarkMode);
-    document.body.classList.toggle('light-mode', !isDarkMode);
-  }, [isDarkMode]);
-
   return (
-<header>
-  <div className="containers">
-    <div className="logo">
-      <Link href="/">
-      CODAXLAB
-      </Link>
-    </div>
-    <div className="links" id="middlelinks">
-      <Link href="/form"> Inspiration</Link>
-      <a href="Services.html">Learn Design</a>
-      <a href="">Hire Designers</a>
-      <a href="">Hire Developer</a>
-      <a href="">Find Work</a>
-    </div>
-<div className="links">
-<span className="material-symbols-outlined" onClick={toggleDarkMode}>
-contrast
-</span>
-<a href="contactus.html" className="reg">
-        Contact Us
-      </a>
-</div>
-
-  </div>
-</header>
-
+    <header>
+      <div className="containers">
+        <div className="logo">
+          <Link href="/">
+            CODAXLAB
+          </Link>
+        </div>
+        <div className="links" id="middlelinks">
+          <Link href="/form">Inspiration</Link>
+          <a href="Services.html">Learn Design</a>
+          <a href="">Hire Designers</a>
+          <a href="">Hire Developer</a>
+          <a href="">Find Work</a>
+        </div>
+        <div className="links">
+          <span className="material-symbols-outlined" onClick={toggleDarkMode}>
+            contrast
+          </span>
+          <a href="contactus.html" className="reg">
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </header>
   )
 }
 
